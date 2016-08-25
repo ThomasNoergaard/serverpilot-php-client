2cc11c5cf250b9cc53378f2e408c99473bef21b9
Thomas Nørgaard <thomas.noergaard@gmail.com>
2016-08-25 02:48:06 +0200

    Small fix to AbstractEntity test

---
+++ tests/Unit/ClientTest.php
@@ -0,0 +1,24 @@
+<?php
+
+class ClientTest extends PHPUnit_Framework_TestCase
+{
+
+    public function setUp()
+    {
+        parent::setUp();
+    }
+    
+    public function tearDown()
+    {
+        parent::tearDown();
+    }
+    
+    /**
+    * @test
+    */
+    public function it_()
+    {
+    
+    }
+
+}
